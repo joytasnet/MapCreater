e2ed93c320759f165f821e7b1184e6c2d2d85b57
yakan <yume_drop@hotmail.com>
2022-04-05 11:39:21 +0900

    Map追加

---
+++ NaganoMain.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class NaganoMain{
+	public static void main(String[] args){
+		System.out.print("幅>");
+		int w = new Scanner(System.in).nextInt();
+		System.out.print("高さ>");
+		int h = new Scanner(System.in).nextInt();
+		char[][] maps = new char[h][w];
+		char[] maps2 ={'森','草','岩','水'};
+		for(int i = 0;i<maps.length;i++){
+			for(int j =0;j<maps[i].length;j++){
+				int r =new Random().nextInt(4);
+				maps[i][j]=maps2[r];
+				System.out.print(maps[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
